17a8bc3e85932ca3c797484d0b5c5fbb132c6ed7
Juan Carlos <juancafe2@gmail.com>
2016-12-21 14:08:56 -0400

    Learned how utf-8, binary, hex in GO

--- hexadecimal/main.go
+++ hexadecimal/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 20000; i++ {
-		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+	for i := 0; i < 500; i++ {
+		fmt.Printf("%d \t %b \t %x  %q \n", i, i, i, i)
 	}
 }
