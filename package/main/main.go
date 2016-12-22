a25f2014244df259187e8eecf6249cb4e9b24130
Juan Carlos <juancafe2@gmail.com>
2016-12-22 10:59:12 -0400

    Redoing lecture

---
+++ package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/juancafe4/GolangTraining/package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
