b8e08b09026be7b4973e9f79f1154194e4f2a59f
Juan Carlos <juancafe2@gmail.com>
2016-12-26 00:30:29 -0400

    closures

---
+++ scope/closure/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+// It returns a function
+func wrapper() func() int {
+	y := 0
+	return func() int {
+		y++
+		return y
+	}
+}
+func main() {
+	//Fucntions are first class
+	x := 0
+	increment := func() int {
+		x++
+		return x
+	}
+	// Here is calling the function returning the inner function
+	// with y = 0
+	increment2 := wrapper()
+
+	fmt.Printf("%v\n", increment())
+	fmt.Printf("%v\n", increment())
+
+	fmt.Printf("%v\n", increment2())
+	fmt.Printf("%v\n", increment2())
+}
