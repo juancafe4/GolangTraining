302e3df54ddfb14633a286ec89c4e4321f29f726
Juan Carlos <juancafe2@gmail.com>
2016-12-26 11:54:52 -0400

    More pointers

--- pointers/main.go
+++ pointers/main.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+func zero(x int) {
+	fmt.Printf("%p\n", &x)
+	x = 0
+}
+
+func zero2(z *int) {
+	fmt.Printf("%p\n", z)
+	*z = 0
+}
 func main() {
 	a := 43
 
@@ -20,4 +29,15 @@ func main() {
 	fmt.Println(a)
 
 	//Using no pointer example
+	//It is passed it value not the address nothing changes
+	x := 5
+	fmt.Printf("%p\n", &x)
+	zero(x)
+	fmt.Println(x)
+
+	// wtih pointers
+	y := 5
+	fmt.Printf("%p\n", &y)
+	zero2(&y)
+	fmt.Println(y)
 }
