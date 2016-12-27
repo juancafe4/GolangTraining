40e7d0c7391dbc0e119014d3f423a03e0087b743
Juan Carlos <juancafe2@gmail.com>
2016-12-26 20:07:25 -0400

    If statement

---
+++ if_statement/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	b := true
+
+	// First expression you can declare a variable
+	// Second expression is the boolean statement
+	if food := "Chocolate"; b {
+		fmt.Println(food)
+	}
+}
