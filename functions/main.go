d7be23ba722454fcaa4e449fe57fa587af5fd125
Juan Carlos <juancafe2@gmail.com>
2017-01-07 23:30:09 -0400

    Exercises function

--- functions/main.go
+++ functions/main.go
@@ -14,6 +14,11 @@ func main() {
 	func() {
 		fmt.Println("I am driving!")
 	}()
+
+	fmt.Println(half(2))
+	fmt.Println(half(1))
+
+	fmt.Println(greatest(2, 5, 34, 2))
 }
 
 // func greet(fname, lname string) string {
@@ -30,4 +35,19 @@ func greet(fname string, lname string) (s string) {
 //multiple returns
 func greet2(fname string, lname string) (string, string) {
 	return fmt.Sprint(fname, lname), fmt.Sprint(fname, lname)
+}
+
+//Ex1 and ex2
+func half(n int) (int, bool) {
+	return n / 2, n%2 == 0
+}
+
+func greatest(n ...int) int {
+	max := 0
+	for _, v := range n {
+		if max <= v {
+			max = v
+		}
+	}
+	return max
 }
