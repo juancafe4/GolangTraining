4328173f39f4693b88e745da4a6a35e3ed118829
Juan Carlos <juancafe2@gmail.com>
2017-01-11 10:38:09 -0400

    Mutex

--- race_conditions/main.go
+++ race_conditions/main.go
@@ -9,6 +9,7 @@ import (
 
 var wg sync.WaitGroup
 var counter int
+var mutex sync.Mutex
 
 func main() {
 	wg.Add(2)
@@ -21,11 +22,14 @@ func main() {
 func incrementor(s string) {
 	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < 20; i++ {
-		x := counter
-		x++
-		time.Sleep(time.Duration(rand.Intn(3)) * time.Millisecond)
-		counter = x
+		time.Sleep(time.Duration(rand.Intn(20)) * time.Millisecond)
+		mutex.Lock()
+		// x := counter
+		// x++
+		// counter = x
+		counter++
 		fmt.Println(s, i, "Counter:", counter)
+		mutex.Unlock()
 	}
 	wg.Done()
 }
