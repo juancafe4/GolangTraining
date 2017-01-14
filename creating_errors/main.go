2c3763e84c442b422cb928f13556367b05b4e0ff
Juan Carlos <juancafe2@gmail.com>
2017-01-13 23:12:04 -0400

    Error with context

--- creating_errors/main.go
+++ creating_errors/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"errors"
+	// "errors"
+	"fmt"
 	"log"
 )
 
+type SqurtError struct {
+	lat, long string
+	err       error
+}
+
+func (n *SqurtError) Error() string {
+	return fmt.Sprintf("A negative value %v %v %v\n", n.lat, n.long, n.err)
+}
+
+// var SqurtError = errors.New("Error negative value")
 func main() {
 	_, err := Squrt(-10)
 	if err != nil {
@@ -14,7 +25,8 @@ func main() {
 
 func Squrt(f float64) (float64, error) {
 	if f < 0 {
-		return -1, errors.New("Negative value for square root")
+		sqrtErr := fmt.Errorf("Negative value for square root %v\n", f)
+		return -1, &SqurtError{"50.2289 N", "99.4656 W", sqrtErr}
 	}
 
 	// Implementation
